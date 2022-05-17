7621d11c5dc171bc9a21beba6b9cea08c3f9a456
Veli Luukkonen <veli.luukkonen@srk.fi>
2022-05-17 10:34:16 +0300

    six library import correction.

--- djangocms_teaser/models.py
+++ djangocms_teaser/models.py
@@ -14,7 +14,8 @@ except ImportError:
         """
         return instance.get_media_path(filename)
 #from cms.utils.compat.dj import python_2_unicode_compatible
-from django.utils.six import python_2_unicode_compatible
+#from django.utils.six import python_2_unicode_compatible
+from six import python_2_unicode_compatible
 
 
 @python_2_unicode_compatible
